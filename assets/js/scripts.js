5fc54d065a85f63f17a605dfd8b082d3a32171c1
Your Name <you@example.com>
2021-04-28 03:08:59 +0300

    New commits

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,63 @@
 /* Custom JavaScript */
+
+(function($) {
+
+    const heroSlider = $('.hero-slider');
+    const heroThumbs = $('.hero-thumbnails');
+
+    heroThumbs.flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        directionNav: false,
+        itemWidth: 300,
+        itemMargin: 5,
+        asNavFor: '#hero-slider'
+    });    
+
+    heroSlider.flexslider({
+        animation: "fade",
+        controlNav: false,
+        directionNav: false,
+        keyboard: true,
+        multipleKeyboard: true,
+        slideshow: true,
+        slideshowSpeed: 2000,
+        sync: '#hero-thumbnails'
+    });  
+
+    /* Partners carousel */
+    const logoCarousel = $('.partners-carousel');
+    
+    logoCarousel.owlCarousel({
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplaySpeed: 500,
+        dots: false,
+        dotsEach: 1,
+        items: 5,
+        loop: true,
+        lazyLoad: true,
+        responsive: {
+            // breakpoint from 480 upwards
+            0: {
+                center: false,
+                items: 1 
+            },
+            700: {
+                items: 2
+            },
+            900: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1200: {
+                items: 5
+            }
+        },        
+    });      
+
+})(jQuery);
--- assets/vendor/owl-carousel/owl.carousel.init.js
+++ assets/vendor/owl-carousel/owl.carousel.init.js
@@ -2,38 +2,6 @@
 
 (function($) {
     
-    const prodCarousel = $('.product-carousel');
-    
-    // Product carousel
-    prodCarousel.owlCarousel({
-        autoplay: true,
-        autoplayHoverPause: true,
-        autoplaySpeed: 500,
-        center: true,
-        dots: false,
-        dotsEach: 1,
-        items: 4,
-        lazyLoad: true,
-        loop: true,
-        responsive: {
-            // breakpoint from 480 upwards
-            0: {
-                center: false,
-                items: 1 
-            },
-            700: {
-                items: 2
-            },
-            900: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            }
-        },        
-    });
+
     
 })(jQuery);
